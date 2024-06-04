db37ca2a802470d71dd2303b98f8ee0e66a6aab4
Nguyen Quang Lam <148853648+ngqulam24@users.noreply.github.com>
2024-06-04 17:40:09 +0700

    Add files via upload

---
+++ Ex1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a number: ");
+        double num = double.Parse(Console.ReadLine());
+
+        if (num > 0)
+        {
+            Console.WriteLine("The number is positive.");
+        }
+        else if (num < 0)
+        {
+            Console.WriteLine("The number is negative.");
+        }
+        else
+        {
+            Console.WriteLine("The number is zero.");
+        }
+    }
+}
---
+++ Ex2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8; //
+        Console.Write("Nhập số của ngày trong tuần (1-7): ");
+        int dayNumber = int.Parse(Console.ReadLine());
+
+        string dayName;
+
+        switch (dayNumber)
+        {
+            case 1:
+                dayName = "Thứ Hai";
+                break;
+            case 2:
+                dayName = "Thứ Ba";
+                break;
+            case 3:
+                dayName = "Thứ Tư";
+                break;
+            case 4:
+                dayName = "Thứ Năm";
+                break;
+            case 5:
+                dayName = "Thứ Sáu";
+                break;
+            case 6:
+                dayName = "Thứ Bảy";
+                break;
+            case 7:
+                dayName = "Chủ Nhật";
+                break;
+            default:
+                dayName = "Không hợp lệ";
+                break;
+        }
+
+        Console.WriteLine("Ngày này là " + dayName);
+    }
+}
